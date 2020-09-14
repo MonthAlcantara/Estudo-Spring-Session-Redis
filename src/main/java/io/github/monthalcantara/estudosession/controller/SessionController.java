b1594e7fea9756df0f846d3500850ac35ba4cb4e
Montival <montival_junior@yahoo.com.br>
2020-09-13 23:15:53 -0300

    Adicionado endpoint para teste de sessao

--- src/main/java/io/github/monthalcantara/estudosession/controller/SessionController.java
+++ src/main/java/io/github/monthalcantara/estudosession/controller/SessionController.java
@@ -1,15 +1,30 @@
 package io.github.monthalcantara.estudosession.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("v1/session")
-public class SessionController {
+public class SessionController extends HttpServlet {
 
-    @GetMapping
-    public String helloWorld() {
-        return "Você está logado na aplicação";
+    @Autowired
+    Jedis j;
+
+
+    @GetMapping("/contador")
+    public String contaRefresh(HttpServletRequest request) {
+        Integer pageViews = 1;
+        if (request.getSession().getAttribute("pageViews") != null) {
+            pageViews += (Integer) request.getSession().getAttribute("pageViews");
+        }
+        request.getSession().setAttribute("pageViews", pageViews);
+
+        return String.valueOf(pageViews);
     }
 }
