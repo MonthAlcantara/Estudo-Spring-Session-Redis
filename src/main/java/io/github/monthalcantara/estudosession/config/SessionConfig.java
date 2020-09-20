165ffa1c2e55a630f81fba3c94e960f228c2d279
Montival <montival_junior@yahoo.com.br>
2020-09-20 13:28:10 -0300

    Substituido Lettuce pelo Jedis

--- src/main/java/io/github/monthalcantara/estudosession/config/SessionConfig.java
+++ src/main/java/io/github/monthalcantara/estudosession/config/SessionConfig.java
@@ -1,14 +1,16 @@
 package io.github.monthalcantara.estudosession.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import redis.clients.jedis.Jedis;
 
 
 @EnableRedisHttpSession
 public class SessionConfig {
+
     @Bean
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+    public Jedis jedisConnectionFactory() {
+        Jedis factory = new Jedis("localhost",6379);
+        return factory;
     }
 }
